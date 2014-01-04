4f3c532f154d5438cd8c9f14c91c74d757f663da
Graham MacDonald <grahamamacdonald@gmail.com>
2014-01-04 09:05:06 +0000

    Dynamically load blog

--- src/chanchantwo/core.cljs
+++ src/chanchantwo/core.cljs
@@ -1,7 +1,8 @@
 (ns chanchantwo.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require-macros [cljs.core.async.macros :refer [go alt!]])
   (:require [goog.events :as events]
-            [cljs.core.async :refer [put! chan <!]]
+            [goog.net.XhrIo :as xhr]
+            [cljs.core.async :as async :refer [put! chan <! close!]]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true])
   (:import [goog.net Jsonp]
@@ -9,63 +10,27 @@
 
 (defn elem [id] (.getElementById js/document id))
 
+(defn log [s] (.log js/console (str s)))
+
 (defn listen [el type]
   (let [out (chan)]
     (events/listen el type
       (fn [e] (put! out e)))
     out))
 
-(defn jsonp [uri]
-  (let [out (chan)
-        req (Jsonp. (Uri. uri))]
-    (.send req nil (fn [res] (put! out res)))
-    out))
+(defn GET [url]
+  (let [ch (chan 1)]
+    (xhr/send url
+              (fn [event]
+                (let [res (-> event .-target .getResponseText)]
+                  (go (>! ch res)
+                      (close! ch)))))
+    ch))
 
-(def wiki-search-url
-  "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=")
-
-(defn query-url [q]
-  (str wiki-search-url q))
-
-(defn user-query []
-  (.-value (elem "query")))
-
-(defn render-query [results]
-  (str
-    "<ul>"
-    (apply str
-      (for [result results]
-        (str "<li>" result "</li>")))
-    "</ul>"))
-
-(defn render-query-om [results]
+(defn widget [contents data]
   (om/component
-    (dom/ul #js {:id "results"}
-            (for [result results]
-              (dom/li nil result)))))
+    (dom/div nil contents)))
 
-(defn widget [data]
-  (om/component
-    (om/ul nil
-           (om/li nil "abc")
-           (om/li nil "def"))))
-;    (dom/div nil "Hello world!")))
-
-
-(defn widget2 [data]
-  (om/component
-    (dom/div nil "Hello world2!")))
-
-(defn init []
-  (let [clicks (listen (elem "search") "click")
-        results-view (elem "results")]
-    (go (while true
-          (let [[_ results] (<! (jsonp (query-url (user-query))))]
-	            (om/root {} (fn [] (render-query-om results)) results-view))))))
-            ;(om/root {} widget results-view))))))
-            ;(set! (.-innerHTML results-view) (render-query results)))))))
-
-(init)
-
-
-(om/root {} widget (elem "xxx"))
+(go
+  (let [blog (<! (GET "http://localhost:8000/blog.md"))]
+    (om/root {} (partial widget blog) (elem "blog-contents"))))
